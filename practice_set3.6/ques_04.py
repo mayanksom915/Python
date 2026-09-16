7f0be5754bd2880da5e14467e125dd31551369e2
Mayank Som <mayanksom915@gmail.com>
2026-09-16 06:09:54 +0530

    Update ques_04.py

--- practice_set3.6/ques_04.py
+++ practice_set3.6/ques_04.py
@@ -1,9 +1,16 @@
 # 4. Write a function maximum(a, b) that returns the larger of two numbers.
 
-def maximum(a,b):
-    print(maximum(int(input("Enter the value of a:- ",a))))
-    print(maximum(int(input("Enter the value of b:- ",b))))
-    if(a>b):
-        print("A is greater")
+
+def maximum(a, b):
+
+    if a > b:
+        return a  
     else:
-        print("B is greater")
+        return b  
+
+num1 = int(input("Enter the value of a: "))
+num2 = int(input("Enter the value of b: "))
+
+larger_number = maximum(num1, num2)
+
+print("The larger number is:", larger_number)
